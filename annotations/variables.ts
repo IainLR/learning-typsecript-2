404260bb70508f2fd09c4e58b4c4338f017269fe
IainLR <iainrobertzon@gmail.com>
2021-04-02 16:11:36 -0400

    type annotation examples

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,4 +1,25 @@
+// Type annotations. we are specifying the type a variable must be
 let apples: number = 5
 let speed: string = 'fast'
 let hasName: boolean = true
+
 let nothingMuch: null = null
+let nothing: undefined = undefined
+
+// built in objects
+let now: Date = new Date()
+
+// Array
+let colors: string[] = ['red', 'green', 'blue']
+let myNumbers: number[] = [9, 8, 7]
+let truths: boolean[] = [true, true, false]
+
+// classes
+class Car {}
+let car: Car = new Car()
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+}
