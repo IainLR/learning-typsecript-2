d306564b1fe6ca984a725e2bdba22e07a13dffdd
IainLR <iainrobertzon@gmail.com>
2021-04-03 16:30:12 -0400

    final example for annotation scenarios

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -30,7 +30,21 @@ const logNumber: (i: number) => void = (i: number) => {
 }
 
 // When to use annotations
-//1) Function that returns the 'any' type
+// 1) Function that returns the 'any' type
 const json = '{"x": 10, "y": 20}'
-const coordinates = JSON.parse(json)
+const coordinates: { x: number; y: number } = JSON.parse(json)
 console.log(coordinates) //{x: 10, y: 20}
+
+// 2) when we declare a variable on one line, but initialize it latter
+let words = ['red', 'green', 'blue']
+let foundWord: boolean
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, -12]
+let numberAboveZero
