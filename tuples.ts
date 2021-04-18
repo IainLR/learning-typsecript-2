5e1a10d1367b44845e25ac77ff8c1f2e74810af5
IainLR <iainrobertzon@gmail.com>
2021-04-18 14:51:01 -0500

    tuples vs objects

--- tuples.ts
+++ tuples.ts
@@ -4,7 +4,18 @@ const drink = {
   sugar: 40,
 }
 
-// type alias, allows us to define custom types
+// type alias, allows us to define custom types for reusability
 type Drink = [string, boolean, number]
 // defining the tuple. Establishing to correct order
-const pepsi: [string, boolean, number] = ['brown', true, 40]
+// const pepsi: [string, boolean, number] = ['brown', true, 40]
+const pepsi: Drink = ['brown', true, 40]
+const sprite: Drink = ['clear', true, 45]
+const tea: Drink = ['brown', false, 0]
+
+//tuples far less explicit as a glance. Objects preferable in general
+const carSpecs: [number, number] = [400, 3354]
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+}
