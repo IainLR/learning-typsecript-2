2ad69fc191f21535a6965b2d6fd3bda908125551
IainLR <iainrobertzon@gmail.com>
2021-04-18 15:33:41 -0500

    inheritance review

--- classes.ts
+++ classes.ts
@@ -1,13 +1,32 @@
 class Vehicle {
-  drive(): void {
+  public drive(): void {
     console.log('vrooom')
   }
 
-  honk(): void {
+  protected honk(): void {
     console.log('beep')
   }
 }
 
-const vehicle = new Vehicle()
-vehicle.drive()
-vehicle.honk()
+class Car extends Vehicle {
+  drive(): void {
+    console.log('new vroom')
+  }
+
+  startDrivingProcess(): void {
+    this.drive()
+    this.honk()
+  }
+}
+
+// const vehicle = new Vehicle()
+// vehicle.drive()
+// vehicle.honk()
+
+const car = new Car()
+car.startDrivingProcess()
+// car.honk()
+
+// public private protected
+// private is often used to protect the application from other developers who may not fully understand the use of a given method/function.
+//protected allows child classes to call methods, but these methods are otherwise inaccessable outside of the classes themselves
